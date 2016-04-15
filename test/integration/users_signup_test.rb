d0885a2e32f22308fe8ef66cd2d1f25df5ae2421
despino <despino13@yahoo.com>
2016-04-14 19:31:37 -0700

    Modified gravatar options

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,8 +11,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
   end
   test "valid signup information" do
     get signup_path
@@ -23,6 +21,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
-    assert_not flash.FILL_IN
   end
 end
